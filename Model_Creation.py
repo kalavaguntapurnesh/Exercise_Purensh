a688645f5edef85db30b510c228aa294c740829d
Purnesh K <111479130+kalavaguntapurnesh@users.noreply.github.com>
2023-02-05 11:54:28 +0530

    Create Model_Creation.py

---
+++ Model_Creation.py
@@ -0,0 +1,48 @@
+pip install imutils
+#imutils is used for basic image processing like translation, rotation etc..
+pip install opencv-python
+#tensorflow usage which is a python library for ML Properties
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+#MobileNetV2 used for the Image Segmentation..
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+# initialize the initial learning rate, number of epochs to train for,
+# and batch size
+INIT_LR = 1e-4
+EPOCHS = 20
+BS = 32
+DIRECTORY = 'dataset/'
+CATEGORIES = ["with_mask", "without_mask"]
+# grab the list of images in our dataset directory, then initialize
+# the list of data (i.e., images) and class images
+print("[INFO] loading images...")
+#used for storing the images in the form of arrays
+data = []
+labels = []
+for category in CATEGORIES:
+    path = os.path.join(DIRECTORY, category)
+    for img in os.listdir(path):
+        img_path = os.path.join(path, img)
+        image = load_img(img_path, target_size=(224, 224))
+        image = img_to_array(image)
+        image = preprocess_input(image)
+
+        data.append(image)
+        labels.append(category)
